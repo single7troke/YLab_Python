f988bfd5cff6b1b97b32900e1491ed58db8986fb
AlexeyDubtsov <alexeydubtsov@gmail.com>
2023-07-25 10:09:03 +0300

    postgress settings were added to config

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,19 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Postgres(BaseSettings):
+    user: str
+    password: int
+    db: str
+    host: str = "db"
+    port: int = 5432
+
+    def url(self):
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+
+    model_config = SettingsConfigDict(env_prefix="postgres_")
 
 
 class Config(BaseSettings):
     app_name: str = "Hello"
-
+    postgres: Postgres = Postgres()
