53a146af795f1671f0d30133a92fcb66dc70ac3c
AlexeyDubtsov <alexeydubtsov@gmail.com>
2023-08-06 13:54:26 +0300

    base_schema

--- app/schemas/base_schema.py
+++ app/schemas/base_schema.py
@@ -14,51 +14,7 @@ class BaseOrJSONModel(BaseModel):
         json_dumps = orjson_dumps
 
 
-class CreateMenu(BaseModel):
-    title: str
-    description: str
-
-
-class CreateDish(CreateMenu):
-    price: str
-
-
 class Base(BaseOrJSONModel):
     id: UUID
     title: str
     description: str
-
-
-class Menu(Base):
-    submenus_count: int = 0
-    dishes_count: int = 0
-
-    @classmethod
-    def fill(cls, data):
-        return cls(id=str(data.id),
-                   title=data.title,
-                   description=data.description,
-                   submenus_count=data.submenus_count,
-                   dishes_count=data.dishes_count)
-
-
-class SubMenu(Base):
-    dishes_count: int = 0
-
-    @classmethod
-    def fill(cls, data):
-        return cls(id=str(data.id),
-                   title=data.title,
-                   description=data.description,
-                   dishes_count=data.dishes_count)
-
-
-class Dish(Base):
-    price: str
-
-    @classmethod
-    def fill(cls, data):
-        return cls(id=str(data.id),
-                   title=data.title,
-                   description=data.description,
-                   price=str(data.price))
